09d71896d02f6d275e00a2c41a0f66bc5827806c
Aslan Lee <aslanlee@aslans-macbook-pro.local>
2018-07-16 12:00:20 -0500

    Added a bunch of buttons

---
+++ PikaClicker Fight/SecondViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SecondViewController.swift
+//  PikaClicker Fight
+//
+//  Created by Aslan Lee on 7/16/18.
+//  Copyright © 2018 Aslan Lee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBAction func pikachuCharacterButton(_ sender: Any) {
+    }
+    @IBAction func squirtleCharacterButton(_ sender: Any) {
+    }
+    @IBAction func charmanderCharacterButton(_ sender: Any) {
+    }
+    @IBAction func eeveeCharacterButton(_ sender: Any) {
+    }
+    @IBAction func bulbasaurCharacterButton(_ sender: Any) {
+    }
+    @IBAction func bellsproutCharacterButton(_ sender: Any) {
+    }
+    @IBAction func rattleCharacterButton(_ sender: Any) {
+    }
+    @IBAction func snorlaxCharacterButton(_ sender: Any) {
+    }
+    @IBAction func meowthCharacterButton(_ sender: Any) {
+    }
+    @IBAction func dratiniCharacterButton(_ sender: Any) {
+    }
+    @IBAction func jigglypuffCharacterButton(_ sender: Any) {
+    }
+    @IBAction func mewCharacterButton(_ sender: Any) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+
+}
