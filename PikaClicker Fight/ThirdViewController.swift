e148b10d7c9fbbdd0b14717054ee61a61dd13ad6
Aslan Lee <aslanlee@aslans-macbook-pro.local>
2018-07-18 17:46:25 -0500

    finished, needs animations

--- PikaClicker Fight/ThirdViewController.swift
+++ PikaClicker Fight/ThirdViewController.swift
@@ -15,9 +15,12 @@ class ThirdViewController: UIViewController {
     var imageName2 = "mew"
     var fightCounter1 = 0
     var fightCounter2 = 0
+    var height = 50
     
     @IBOutlet weak var fighter1Outlet: UIButton!
     @IBOutlet weak var fighter2Outlet: UIButton!
+    @IBOutlet weak var player1Line: UILabel!
+    @IBOutlet weak var player2Line: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +29,7 @@ class ThirdViewController: UIViewController {
         fighter1Outlet.setImage(UIImage(named: imageName), for: .normal)
         fighter2Outlet.setImage(UIImage(named: imageName2), for: .normal)
         fighter1Outlet.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI))
+        
     }
     
     func showAlert(winner: String) // makes an alert for the winner
@@ -42,16 +46,38 @@ class ThirdViewController: UIViewController {
     @IBAction func fighter1(_ sender: Any) {
     fightCounter1 += 1
         checkForWinner()
+//        height += 10
+//        var label = UILabel(frame: CGRect(x: 0, y: 0, width: 25, height: height))
+//        label.backgroundColor = .red
+        let rand = Int(arc4random_uniform(312))
+        let label = UILabel(frame: CGRect(x: rand, y: 199, width: 63, height: 59))
+        label.center = CGPoint(x: rand, y: 285)
+        label.textAlignment = .center
+        label.text = "⚡" //make a variable, then make if statement (if image == pikachuSmiling)
+        label.isHidden = true // hides the label till animation starts
+        self.view.addSubview(label)
+        label.center.y -= view.bounds.width
+        label.isHidden = false // its there!
+        UIView.animate(withDuration: 2.0, animations: {
+            label.center.y += self.view.bounds.width
+        })
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+            UILabel.animate(withDuration: 1) {
+                label.alpha = 0
+            }
+        })
     }
     
     @IBAction func fighter2(_ sender: Any) {
     fightCounter2 += 1
         checkForWinner()
+        height += 5
+        
     }
     
     func checkForWinner()
     {
-        if fightCounter1 == 5{
+        if fightCounter1 == 200{
             showAlert(winner: imageName)
         }
         if fightCounter2 == 5{
